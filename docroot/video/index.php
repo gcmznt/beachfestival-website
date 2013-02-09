619b02b351ef711445dd655cc417e43ac5b339e8
Giacomo Zinetti <giacomo.zinetti@giko.it>
2013-02-09 14:48:13 +0100

    video popup

---
+++ docroot/video/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="it">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+    <title>FTBI Beach TchoukBall Festival</title>
+    
+    <link rel="stylesheet" type="text/css" href="/resources/normalize.css" />
+    
+    <script type="text/javascript">
+        // var _gaq = _gaq || [];
+        // _gaq.push(['_setAccount', 'UA-38267535-1']);
+        // _gaq.push(['_trackPageview']);
+
+        // (function() {
+        //     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        //     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        //     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        // })();
+    </script>
+</head>
+<body>
+
+<iframe width="640" height="480" src="http://www.youtube.com/embed/<?php echo $_GET['v']; ?>?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+
+</body>
